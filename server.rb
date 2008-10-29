01569550fe632bc76fef59bfd2cab20079a78d85
Colin Campbell-McPherson <colin@logaan.net>
2008-10-29 23:01:33 +0900

    refactored server code for readability added comments explaining component methods

--- server.rb
+++ server.rb
@@ -2,29 +2,56 @@
 
 require 'rubygems'
 require 'rack'
-require 'pp'
 
+# The HamWiki class presents an interface to rack and handles all requests
 class HamWiki
+  # Documents are stored in the documents directory within the HAMwiki project
+  DOCUMENTS_PATH = File.join(File.dirname(__FILE__), "documents")
+
+  # Rack will call call every time a request is made. Our call method will
+  # simply pick between serving up the root wiki page or an individual document
   def call env
-    case env["PATH_INFO"]
-      when "/"
-        [200, {"Content-Type" => "text/html"},
-          [File.read(File.join(File.dirname(__FILE__), "client.html"))]]
-      else
-        document = File.join(File.dirname(__FILE__), "documents", env["PATH_INFO"])
-        
-        if File.file?(document) && File.readable?(document)
-          [200, {
-             "Last-Modified"  => File.mtime(document).rfc822,
-             "Content-Type"   => "text/plain",
-             "Content-Length" => File.size(document).to_s
-           }, File.read(document)]
-        else
-          [404, {"Content-Type" => "text/plain"},
-            ["404: File not found.\n"]]
-        end
+    if env["PATH_INFO"] == "/"
+      root_response
+    else
+      retrieve_document env["PATH_INFO"]
     end
+  end
+
+  # If they're requesting a document we'll serve it if it's available, or give
+  # a not found response
+  def retrieve_document path_info
+    document = File.join(DOCUMENTS_PATH, path_info)
+    if File.file?(document) && File.readable?(document)
+      serve_document document
+    else
+      not_found
+    end
+  end
+
+  # When someone accesses the root they're served up the client.html file. This
+  # contains the javascript code that'll grab and format the plain text
+  # documents and allow for navigation between then.
+  def root_response
+    [200, {"Content-Type" => "text/html"},
+      [File.read(File.join(File.dirname(__FILE__), "client.html"))]]
+  end
+
+  # A successful http response with the document contents and meta-data
+  def serve_document document
+      [200, {
+         "Last-Modified"  => File.mtime(document).rfc822,
+         "Content-Type"   => "text/plain",
+         "Content-Length" => File.size(document).to_s
+       }, File.read(document)]
+  end
+
+  # A 404 not found http response
+  def not_found_response
+    [404, {"Content-Type" => "text/plain"},
+      ["404: File not found.\n"]]
   end
 end
 
+# Bootstrap the HamWiki class
 Rack::Handler::WEBrick.run(Rack::Lint.new(HamWiki.new), :Port => 9292)
